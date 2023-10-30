59490913bd6bd180e26e27d032bd38b9935fd276
FrancoRumiky <framqoo@gmail.com>
2023-10-30 20:59:43 -0300

    about modif and index.html

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -18,25 +18,17 @@ export default function About() {
             más de 30 años en el mundo de la pintura. Desde nuestros humildes
             comienzos, hemos dedicado nuestra pasión y habilidad a transformar
             hogares y espacios comerciales en obras maestras de color y diseño.
-            Nuestro compromiso con la excelencia y la atención al detalle nos ha
-            convertido en una elección de confianza para innumerables clientes
-            en busca de servicios de pintura de alta calidad. Como empresa
-            familiar, nos enorgullece mantener la integridad y los valores que
-            han sido el corazón de nuestro negocio desde el principio. Nuestro
-            equipo de pintores altamente capacitados y experimentados se dedica
-            a brindar resultados excepcionales. Utilizamos solo los mejores
-            materiales y técnicas de vanguardia para asegurarnos de que cada
-            proyecto se realice con precisión y cuidado. Ya sea que esté
-            buscando dar vida a su hogar con un nuevo esquema de colores o
-            renovar su espacio comercial, estamos aquí para hacer que su visión
-            cobre vida. En nuestra empresa, no solo pintamos paredes; creamos
-            ambientes. Nuestra pasión por el arte de la pintura se refleja en
-            cada proyecto que emprendemos, y estamos ansiosos por compartir esa
-            pasión con usted. Confíe en nosotros para brindarle resultados
-            impecables que transformarán su espacio en algo excepcional. Gracias
-            por considerar a nuestra empresa familiar de pintores para sus
-            necesidades de pintura. Esperamos con interés trabajar con usted y
-            hacer que su proyecto sea un éxito".
+            <br />
+            Especializados en pintura y revestimiento de piletas tanto de fibra
+            como de material, nuestros trabajos reflejan la pasion por el arte
+            de la pintura combinado con un profundo conocimiento de las técnicas
+            y materiales más avanzados en el sector. La satisfacción de nuestros
+            clientes es nuestra principal prioridad, y nuestro compromiso con la
+            calidad y el detalle se refleja en cada proyecto que emprendemos.
+            <br />
+            Confíe en nuestra experiencia y dedicación para transformar su
+            espacio acuático en un oasis de belleza y funcionalidad. Su piscina
+            merece lo mejor, y eso es exactamente lo que ofrecemos."
           </p>
         </div>
         <div className={s.waves}>
